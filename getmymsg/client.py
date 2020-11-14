6f9abc102d7cac65f1ff962dc3468286937694aa
Kiddo <pjfariakiddo@gmail.com>
2020-11-14 19:50:08 -0400

    Arreglos

--- getmymsg/client.py
+++ getmymsg/client.py
@@ -204,7 +204,7 @@ class Client():
 		try:
 			self.client_sock.send((Client.GIVEMEMSG_CMD + str(self.config.bind_port_h)).encode())
 			logging.info('Longitud de mensaje: ' + self.msglen)
-			data, addr = self.msgudp_sock.recvfrom(int(self.msglen))
+			data, addr = self.msgudp_sock.recvfrom(int(self.msglen) + 10)
 			info = data.decode('utf-8').strip('\n').split(' ')
 			self.validate_msg(info[0])
 			self.msgudp_sock.close()
