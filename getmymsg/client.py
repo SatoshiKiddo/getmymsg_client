ff1a361ae494b4f07a7c4647408d0337d106a8e1
Kiddo <pjfariakiddo@gmail.com>
2020-11-14 18:05:50 -0400

    Reparacion cliente

--- getmymsg/client.py
+++ getmymsg/client.py
@@ -122,10 +122,11 @@ class Client():
         	logging.info('Iniciando cliente en %s:%s' % addr_port)
         	trys = 0
 		try:
-			self.msgudp_sock.bind(addr_port_h)
+			# self.msgudp_sock.bind(addr_port_h)
 			self.conn = self.client_sock.connect(addr_port)
-		except:
+		except as error:
 			logging.warn('Se produjo un error de conexion inicial desde %s:%s' % addr_port)
+			logging.warn(error)
 			raise ConnectionException()
 		self.__working = True
 		logging.info('Conexion creada correctamente')
